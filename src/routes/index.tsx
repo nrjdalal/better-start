7b7e39a9f48b01bf2e47e85a0f5dacca7007964f
Neeraj Dalal <admin@nrjdalal.com>
2025-06-03 04:51:25 +0530

    chore: tweaks > index.tsx
    src/routes/index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,19 @@ export const Route = createFileRoute({
 
 function Component() {
   return (
-    <main className="flex min-h-dvh w-screen flex-col items-center justify-center gap-y-4 p-4">
+    <main className="flex min-h-dvh flex-col items-center justify-center gap-y-4 bg-radial from-cyan-950 to-black p-4 text-gray-100">
       <img
         className="aspect-square w-full max-w-sm"
         src="https://tanstack.com/assets/splash-dark-8nwlc0Nt.png"
         alt="TanStack Logo"
       />
-      <h1>TanStack Start</h1>
+      <h1 className="text-2xl">
+        <span className="font-semibold">TanStack</span>
+        &nbsp;
+        <span className="text-cyan-500">Start</span>
+      </h1>
       <a
-        className="rounded-full border px-4 py-1 hover:opacity-90"
+        className="rounded-full bg-gray-100 px-4 py-1 text-gray-900 hover:opacity-90"
         href="https://tanstack.com/start/latest"
         target="_blank"
       >
