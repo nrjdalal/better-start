76151e2ab7d5a59c1a24b324b0b215951822b7a2
Neeraj Dalal <admin@nrjdalal.com>
2025-06-03 08:04:43 +0530

    chore: tweaks > auth.tsx
    src/components/auth.tsx

--- src/components/auth.tsx
+++ src/components/auth.tsx
@@ -43,6 +43,7 @@ export default function Component() {
         onClick={async () =>
           await signIn.social({
             provider: "github",
+            callbackURL: "/dashboard",
           })
         }
       >
